a74c6b91be5d263f883032627ada81d79bd9d51e
Vlad-Savch <vladrest1213@gmail.com>
2024-10-18 00:55:28 +0300

    Реализованы функции multiplicative_inverse() и generate_keypair()

--- RSA_generatekey.py
+++ RSA_generatekey.py
@@ -10,11 +10,56 @@ def is_prime(n: int) -> bool:
             return False
     return True
 
+
 def gcd(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
     return a
 
 
+def multiplicative_inverse(e: int, phi: int) -> int:
+    def extended_gcd(a: int, b: int):
+        if b == 0:
+            return a, 1, 0
+        gcd, x1, y1 = extended_gcd(b, a % b)
+        x = y1
+        y = x1 - (a // b) * y1
+        return gcd, x, y
+
+    gcd, x, _ = extended_gcd(e, phi)
+    if gcd != 1:
+        raise ValueError("No multiplicative inverse exists")
+    return x % phi
+
+
+import random
+from typing import Tuple
+
+
+def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    n = p * q
+
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
 
 
