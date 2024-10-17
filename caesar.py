a1e3510581bc56ad639aeaf9f28268ac559efa96
Vlad-Savch <vladrest1213@gmail.com>
2024-10-18 00:50:11 +0300

    Реализована функция is_prime(n)

---
+++ caesar.py
@@ -0,0 +1,48 @@
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+            Encrypts plaintext using a Caesar cipher.
+
+            >>> encrypt_caesar("PYTHON")
+            'SBWKRQ'
+            >>> encrypt_caesar("python")
+            'sbwkrq'
+            >>> encrypt_caesar("Python3.6")
+            'Sbwkrq3.6'
+            >>> encrypt_caesar("")
+            ''
+            """
+
+    ciphertext = ""
+    for char in plaintext:
+        if 'A' <= char <= 'Z':
+            ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        elif 'a' <= char <= 'z':
+            ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += char
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+        Decrypts a ciphertext using a Caesar cipher.
+
+        >>> decrypt_caesar("SBWKRQ")
+        'PYTHON'
+        >>> decrypt_caesar("sbwkrq")
+        'python'
+        >>> decrypt_caesar("Sbwkrq3.6")
+        'Python3.6'
+        >>> decrypt_caesar("")
+        ''
+        """
+
+    plaintext = ""
+    for char in ciphertext:
+        if 'A' <= char <= 'Z':
+            plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+        elif 'a' <= char <= 'z':
+            plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += char
+    return plaintext
